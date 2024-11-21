bf324a07fa0e89686e0a2334554b0d49227e7cbf
Gil Yoder <gil.yoder@oabs.org>
2024-11-21 15:52:07 -0600

    Create App.key.cs
    I added a partial class file to show developers how to add a license key to the code to compile it with Syncfusion support.

---
+++ R8LocoCtrl/App.key.cs
@@ -0,0 +1,16 @@
+using System.Windows;
+
+namespace R8LocoCtrl
+{
+    public partial class App : Application
+    {
+        // R8C uses several libraries from Syncfusion which are available on
+        // NuGet but still require a license from Syncfusion to use. Some
+        // users may qualify for a free license for their "Community Version"
+        // at https://www.syncfusion.com/sales/communitylicense, others may
+        // be required to purchase a license for its use. Please check with
+        // Syncfusion to determine what you require and abide by their
+        // requirements.
+        private const string SFKEY  = "ADD YOUR SYNCFUSION LICENSE KEY HERE";
+    }
+}
