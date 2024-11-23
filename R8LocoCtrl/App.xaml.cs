c6814ae2ad7a108faaa1959327745247755ec28e
Gil Yoder <gil.yoder@oabs.org>
2024-11-23 09:50:15 -0600

    Deleted App.keys.cs. Rewrote App.xaml.cs to get Syncfusion Key from an environment variable.

--- R8LocoCtrl/App.xaml.cs
+++ R8LocoCtrl/App.xaml.cs
@@ -15,6 +15,11 @@ namespace R8LocoCtrl
     {
         public App()
         {
+            // A license key from Syncfusion is required to compile and use
+            // the Syncfusion components in the application. To keep the
+            // license key secure we get the key from an environment
+            // variable.
+            var SFKEY = Environment.GetEnvironmentVariable("SFKEY");
             Syncfusion.Licensing.SyncfusionLicenseProvider
                 .RegisterLicense(SFKEY);
         }
