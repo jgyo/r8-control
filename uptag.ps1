61b870ac059e81d7076712d3e7dbb83dc3258ef3
Gil Yoder <gil.yoder@oabs.org>
2024-12-06 09:04:32 -0600

    Renamed Syncfusion license key secret name.

--- uptag.ps1
+++
@@ -1,7 +0,0 @@
-# uptag.ps1
-# Run this to enable: Set-ExecutionPolicy -ExecutionPolicy Unrestricted -scope CurrentUser
-$tag = $args[0]
-Write-Host "Setting tag v$tag named Release $tag"
-$ok = Read-Host "Ok?"
-git tag -a v$tag -m "Release $tag"
-git push origin v$tag
